69118efd88f650e8b3b020792af391e90f57f16a
Emerald14628 <emerald14628@gmail.com>
2025-08-09 19:05:12 -0500

    introduce field centric driving and new FCD telop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,19 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 public class DriveSubsystem {
     private final DcMotor frontLeftMotor;
     private final DcMotor backLeftMotor;
     private final DcMotor frontRightMotor;
     private final DcMotor backRightMotor;
+    private final IMU imu;
+    private boolean fieldCentric = true;
 
-    public DriveSubsystem(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight) {
+    public DriveSubsystem(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, IMU imu) {
         this.frontLeftMotor = frontLeft;
         this.backLeftMotor = backLeft;
         this.frontRightMotor = frontRight;
         this.backRightMotor = backRight;
+        this.imu = imu;
+    }
+
+    public void setFieldCentric(boolean enabled) {
+        fieldCentric = enabled;
+    }
+
+    public boolean isFieldCentric() {
+        return fieldCentric;
     }
 
     public void handleDriveInput(double y, double x, double rx, double leftTrigger, double rightTrigger) {
@@ -21,17 +35,33 @@ public class DriveSubsystem {
 
         // Apply speed modifications
         y = y * maxPower;
-        x = x * maxPower * 1.1; // Keeping the original 1.1 multiplier for x
+        x = x * maxPower;
         rx = rx * maxPower;
 
-        // Calculate motor powers using the denominator method
+        // Get the robot's heading from the IMU
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        double botHeading = orientation.getYaw(AngleUnit.RADIANS);
+
+        // If field centric is enabled, adjust input vectors based on robot heading
+        if (fieldCentric) {
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            x = rotX;
+            y = rotY;
+        }
+
+        // Calculate motor powers using mecanum drive kinematics
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
 
-        setMotorPowers(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
+        // Set motor powers
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
     }
 
     private double calculateMaxPower(double leftTrigger, double rightTrigger) {
@@ -52,10 +82,20 @@ public class DriveSubsystem {
         return maxPower;
     }
 
-    private void setMotorPowers(double fl, double bl, double fr, double br) {
-        frontLeftMotor.setPower(-fl);
-        backLeftMotor.setPower(-bl);
-        frontRightMotor.setPower(fr);
-        backRightMotor.setPower(br);
+    /**
+     * Calibrates the IMU by resetting its heading to zero.
+     * Call this method when the robot is placed in its starting orientation.
+     */
+    public void calibrateIMU() {
+        imu.resetYaw();
+    }
+
+    /**
+     * Gets the current heading of the robot in degrees
+     * @return heading in degrees (-180 to 180)
+     */
+    public double getHeading() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
     }
 }
